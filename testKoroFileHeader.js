e572248871d2e317b27c47d9a31ac8c1341ac685
邱南亚 <1766226354@qq.com>
2021-02-23 10:23:05 +0800

    await使用说明

---
+++ testKoroFileHeader.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-23 10:00:32
+ * @LastEditTime: 2021-02-23 10:02:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \javascript-demo\testKoroFileHeader.js
+ */
+
+/**
+ * KoroFileHeader 注解使用快捷键： ctrl + alt + i
+ */
